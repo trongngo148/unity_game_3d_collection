fc0b284a8b09b7dff55c04b43e5fda579991cfe1
Jonathan.Ngo <ngohuutrong.jonathan@outlook.com.vn>
2022-06-06 23:03:55 +0700

    add movement for character

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float speedRotation;
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {        
+        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
+
+
+        if (horizontalInput > 0.2 || horizontalInput < -0.2 || verticalInput > 0.2 || verticalInput < -0.2)
+        {
+            transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+        }
+        
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, speedRotation * Time.deltaTime);
+        }
+    }
+}
