92e071e36ef9007951cc4d8cfaba88b716ba2744
Jonathan.Ngo <ngohuutrong.jonathan@outlook.com.vn>
2022-06-10 01:19:46 +0700

    adding state for movement

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Character: MonoBehaviour {
+    [Header("Controls")]
+    public float playerSpeed = 5.0f;
+    public float crouchSpeed = 2.0f;
+    public float sprintSpeed = 7.0f;
+    public float jumpHeight = 0.8f;
+    public float gravityMultiplier = 2;
+    public float rotationSpeed = 5f;
+    public float crouchColliderHeight = 1.35f;
+
+    [Header("Animation Smoothing")]
+    [Range(0, 1)]
+    public float speedDampTime = 0.1f;
+    [Range(0, 1)]
+    public float velocityDampTime = 0.9f;
+    [Range(0, 1)]
+    public float rotationDampTime = 0.2f;
+    [Range(0, 1)]
+    public float airControl = 0.5f;
+
+    public StateMachine movementSM;
+
+    [HideInInspector]
+    public float gravityValue = -9.81f;
+    [HideInInspector]
+    public float normalColliderHeight;
+    [HideInInspector]
+    public CharacterController controller;
+    [HideInInspector]
+    public PlayerInput playerInput;
+    [HideInInspector]
+    public Transform cameraTransform;
+    [HideInInspector]
+    public Animator animator;
+    [HideInInspector]
+    public Vector3 playerVelocity;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        playerInput = GetComponent<PlayerInput>();
+        cameraTransform = Camera.main.transform;
+
+        movementSM = new StateMachine();
+
+        movementSM.Initialize(stat)
+    }
+}
---
+++ Assets/Scripts/State/StandingState.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class StandingState: State
+{
+    float gravityValue;
+    bool jump;
+    bool crouch;
+    Vector3 currentVelocity;
+    bool grounded;
+    bool sprint;
+    float playerSpeed;
+
+    Vector3 cVelocity;
+
+    public StandingState(Character _character, StateMachine _stateMachine) : base(_character, _stateMachine)
+    {
+        character = _character;
+        stateMachine = _stateMachine;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void HandleInput()
+    {
+        base.HandleInput();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
